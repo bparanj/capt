e086fb49c2c6bbbfc8c88f3e4b672887f4dd431d
Ubuntu <ubuntu@ip-172-31-40-21.us-west-2.compute.internal>
2024-04-21 17:07:41 +0000

    Resolve Blocked hosts error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "clickplan.net"
+  config.hosts << "www.clickplan.net"  # If you also want to explicitly allow requests to www subdomain
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
