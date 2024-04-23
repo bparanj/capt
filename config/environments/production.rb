9a4dcc3517cbc55732b529c9d3dbdc98e13a5ed2
Ubuntu <ubuntu@ip-172-31-40-21.us-west-2.compute.internal>
2024-04-23 15:56:59 +0000

    Log to a file instead of standard out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,10 +51,11 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
-  # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  # config/environments/production.rb
+  config.log_level = :info  # or change to :debug for more verbose logging
+  
+  # Ensure that Rails logs to a file in production.
+  config.logger = ActiveSupport::Logger.new("log/production.log")
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
